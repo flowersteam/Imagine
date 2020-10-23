108f38fdd485f0009f58ee8251d2a46e7b7a49d6
tristan-ka <tristan.karch@gmail.com>
2020-10-23 16:34:22 +0200

    added notebook util functions for animation generation and tsne plot

--- src/utils/notebook_utils.py
+++ src/utils/notebook_utils.py
@@ -1,16 +1,18 @@
 import argparse
 import json
 import sys
-
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import torch
+import pygame
 import numpy as np
 
 sys.path.append('../../../')
-from src.utils.util import set_global_seeds
+
 import src.imagine.experiment.config as config
-from src.imagine.interaction import RolloutWorker
+
 from src.imagine.goal_sampler import GoalSampler
-from src.playground_env.reward_function import get_reward_from_state
-from src.playground_env.descriptions import generate_all_descriptions
+
 
 
 def get_params_for_notebook(path):
@@ -70,3 +72,207 @@ def get_modules_for_notebook(path, params):
 
     policy.load_params(POLICY_FILE)
     return policy_language_model, reward_language_model, policy, reward_function, goal_sampler
+
+
+def generate_animation_reward_module(env, goal_str, reward_language_model, reward_function, policy):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2)
+
+    plots = []
+    rewards1 = []
+    rewards2 = []
+    rewards3 = []
+    ## Rollout of a policy
+    env.reset()
+    initial_o = env.unwrapped.reset_with_goal(goal_str)
+    o = initial_o.copy()
+    goal_encoding = reward_language_model.encode(goal_str)
+    input_goal = torch.tensor(goal_encoding).float().view(1, len(goal_encoding))
+    objects = [obj.object_descr['colors'] + ' ' + obj.object_descr['types'] for obj in env.objects]
+
+    for t in range(20):
+        action = policy.get_actions(o, goal_encoding)
+        o, _, _, _ = env.step(action)
+        input_o = torch.tensor(o).float().view(1, len(o))
+        reward_per_object = reward_function.reward_function.compute_logits_before_or(input_o, input_goal)
+        r1, r2, r3 = (elem.detach()[0][0].item() for elem in reward_per_object)
+        rewards1.append(r1)
+        rewards2.append(r2)
+        rewards3.append(r3)
+        env.render(close=True)
+        obs = pygame.surfarray.array3d(env.viewer).transpose([1, 0, 2])
+        im = ax1.imshow(obs, animated=True)
+        line1, = ax2.plot(rewards1, color='blue', label='R for ' + objects[0])
+        line2, = ax2.plot(rewards2, color='red', label='R for ' + objects[1])
+        line3, = ax2.plot(rewards3, color='green', label='R for ' + objects[2])
+        if t == 0:
+            ax2.legend()
+        plots.append([im, line1, line2, line3])
+
+    ani = animation.ArtistAnimation(fig, plots, interval=400, blit=False,
+                                    repeat=False)
+    return ani
+
+def generate_animation_policy_module(env, goal_str, reward_language_model, policy):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2)
+
+    plots = []
+    z_list1 = []
+    z_list2 = []
+    z_list3 = []
+    ## Rollout of a policy
+    env.reset()
+    initial_o = env.unwrapped.reset_with_goal(goal_str)
+    o = initial_o.copy()
+    goal_encoding = reward_language_model.encode(goal_str)
+    input_goal = torch.tensor(goal_encoding).float().view(1, len(goal_encoding))
+    objects = [obj.object_descr['colors'] + ' ' + obj.object_descr['types'] for obj in env.objects]
+
+    for t in range(20):
+        action = policy.get_actions(o, goal_encoding)
+        o, _, _, _ = env.step(action)
+        input_o = torch.tensor(o).float().view(1, len(o))
+        norm_per_object = policy.actor_network.get_norm_per_object(input_o, input_goal)
+        z1, z2, z3 = (elem.detach().item() for elem in norm_per_object)
+        z_list1.append(z1)
+        z_list2.append(z2)
+        z_list3.append(z3)
+        env.render(close=True)
+        obs = pygame.surfarray.array3d(env.viewer).transpose([1, 0, 2])
+        im = ax1.imshow(obs, animated=True)
+        line1, = ax2.plot(z_list1, color='blue', label='|z| for ' + objects[0])
+        line2, = ax2.plot(z_list2, color='red', label='|z| for ' + objects[1])
+        line3, = ax2.plot(z_list3, color='green', label='|z| for ' + objects[2])
+        if t == 0:
+            ax2.legend()
+        plots.append([im, line1, line2, line3])
+
+    ani = animation.ArtistAnimation(fig, plots, interval=400, blit=False,
+                                    repeat=False)
+    return ani
+
+
+def plot_tsne(X_embedded, descr, code, params):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    sc = ax.scatter(X_embedded[:, 0], X_embedded[:, 1], picker=5)
+    normal_colors = [[0, 0.447, 0.7410], [0.85, 0.325, 0.098], [0.466, 0.674, 0.188], [0.929, 0.694, 0.125],
+                     [0.494, 0.1844, 0.556], [0, 0.447, 0.7410], [0.3010, 0.745, 0.933], [0.85, 0.325, 0.098],
+                     [0.466, 0.674, 0.188], [0.929, 0.694, 0.125],
+                     [0.3010, 0.745, 0.933], [0.635, 0.078, 0.184]]
+    colors_colors = ((171 / 255, 16 / 255, 16 / 255), (0, 81 / 255, 159 / 255), (10 / 255, 145 / 255, 10 / 255))
+
+    def color_code(x):
+        if x == 'colors':
+            firsts = [None] * 4
+            colors = ('red', 'blue', 'green')
+            groups = [None] * len(descr)
+            for i_d, d in enumerate(descr):
+                found = False
+                for i in range(len(colors)):
+                    if colors[i] in d:
+                        if firsts[i] == None:
+                            firsts[i] = i_d
+                        groups[i_d] = i
+                        found = True
+                        break
+                if not found:
+                    if firsts[3] == None:
+                        firsts[3] = i_d
+
+            colors = []
+            for i in range(len(descr)):
+                if groups[i] is None:
+                    colors.append('k')
+                else:
+                    colors.append(colors_colors[groups[i]])
+            sc.set_facecolor(colors)
+            legend = ('red', 'blue', 'green', 'any')
+            plots = [plt.scatter(X_embedded[firsts[i], 0],
+                                 X_embedded[firsts[i], 1],
+                                 c=(list(colors_colors) + ['k'])[i]) for i in range(4)]
+            plt.legend(plots, legend,
+                       loc='upper center',
+                       bbox_to_anchor=(0.5, 1.15),
+                       ncol=4,
+                       fancybox=True,
+                       shadow=True,
+                       prop={'size': 10},
+                       markerscale=2., )
+        elif x == 'predicates':
+            firsts = [None] * 3
+            predicates = ('Grasp', 'Go', 'Grow')
+            groups = [None] * len(descr)
+            for i_d, d in enumerate(descr):
+                for i in range(len(predicates)):
+                    if predicates[i] in d:
+                        if firsts[i] == None:
+                            firsts[i] = i_d
+                        groups[i_d] = i
+                        break
+            colors = []
+            for i in range(len(descr)):
+                if groups[i] is None:
+                    colors.append('k')
+                else:
+                    colors.append(colors_colors[groups[i]])
+            sc.set_facecolor(colors)
+            legend = ('Grasp', 'Go', 'Grow')
+            plots = [plt.scatter(X_embedded[firsts[i], 0],
+                                 X_embedded[firsts[i], 1],
+                                 c=colors_colors[i]) for i in range(3)]
+            plt.legend(plots, legend,
+                       loc='upper center',
+                       bbox_to_anchor=(0.5, 1.15),
+                       ncol=3,
+                       fancybox=True,
+                       shadow=True,
+                       prop={'size': 10},
+                       markerscale=2., )
+        elif x == 'categories':
+            cats = sorted(list(params['env_params']['categories'].keys()))
+            cats.remove('living_thing')
+            categories = params['env_params']['categories']
+            firsts = [None] * (len(categories) + 1)
+
+            groups = [None] * len(descr)
+            for i_d, d in enumerate(descr):
+                found = False
+                for i, cat in enumerate(cats):
+                    is_cat = any([w_type in d for w_type in categories[cat]])
+                    if is_cat:
+                        if firsts[i] == None:
+                            firsts[i] = i_d
+                        groups[i_d] = i
+                        found = True
+                        break
+                if not found:
+                    if firsts[-1] == None:
+                        firsts[-1] = i_d
+
+            colors = []
+            for i in range(len(descr)):
+                if groups[i] is None:
+                    colors.append('k')
+                else:
+                    colors.append(normal_colors[groups[i]])
+            sc.set_facecolor(colors)
+            legend = cats + ['None']
+            plots = [plt.scatter(X_embedded[firsts[i], 0],
+                                 X_embedded[firsts[i], 1],
+                                 c=normal_colors[i]) for i in range(len(cats) + 1)]
+            plt.legend(plots, legend,
+                       loc='upper center',
+                       bbox_to_anchor=(0.5, 1.15),
+                       ncol=3,
+                       fancybox=True,
+                       shadow=True,
+                       prop={'size': 10},
+                       markerscale=2., )
+        else:
+            raise NotImplementedError
+
+    color_code(code)
